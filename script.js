6691bd2a58a6ca46afa6476e151fd613cde3af97
Christina Asquith <christinaasquith@gmail.com>
2021-01-31 15:35:40 -0500

    Complete draft of Hack Club workshop

---
+++ script.js
@@ -0,0 +1,35 @@
+const text= document.querySelector('.text') 
+const secretDiv = document.querySelector('.secret-div')
+
+const keys =[]
+const secretCode = ['h', 'a', 'c', 'k']
+
+const confettiSettings = {
+    target: 'my-canvas',
+    size: 1.4,
+    max: 150
+}
+const confetti = new ConfettiGenerator(confettiSettings)
+
+function keysDetector (e) {
+    console.log(e.key)
+    keys.push(e.key)
+    if (keys.length > secretCode.length) {
+        keys.shift()
+    }
+    if (JSON.stringify(keys) === JSON.stringify(secretCode)) {
+        secretDiv.getElementsByClassName.display = 'block'
+        text.innerHTML = 'Identity Verified!'
+        confetti.render()
+        AnimationEffect({
+            targets: 'body',
+            rotate: 'lturn',
+            backgroundColor: '#fcba03',
+            duration: 2000,
+        })
+    }
+}
+
+window.addEventListener('keyup', keysDetector)
+
+ 
